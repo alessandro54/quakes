d43ff6d6acdba6aa453a47baa77fa45c28a38a3e
Alessandro <par_214@hotmail.com>
2024-05-22 14:50:59 -0500

    :recycle: reordered project structure

---
+++ cmd/quakes/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/alessandro54/quakes/internal/routes"
+)
+
+func main() {
+	router := routes.NewRouter()
+
+	port := 8080
+	addr := fmt.Sprintf(":%d", port)
+	fmt.Printf("Listening on %s...\n", addr)
+
+	err := http.ListenAndServe(addr, router)
+	if err != nil {
+		panic(err)
+	}
+}
